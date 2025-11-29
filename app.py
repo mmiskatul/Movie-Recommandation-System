3a82f62e8995fe8ff3fed1f7eeb4dcdaf0a92fa4
mmiskatul <masabimiskat@gmail.com>
2025-11-29 19:10:21 +0600

    add button 'show recommend'

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st 
 import pandas as pd 
+import pickle 
+
+movies =pickle.load(open("movies_list.pkl",'rb'))
 
 
-st.header("Movie Recomender System")
+st.header("Movie Recomender System") 
+
+st.selectbox("Select the movies from dropdown",movies) 
+
+
+if st.button("show Recommend") :
+    pass
+ 
